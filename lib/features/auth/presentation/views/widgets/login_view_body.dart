b3abb8e9244211206ff09bbd935befce49d653df
OMAR\omark <omarkhaled2219@gmail.com>
2024-10-10 00:00:58 +0300

    first commit

---
+++ lib/features/auth/presentation/views/widgets/login_view_body.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub/constants.dart';
+import 'package:fruits_hub/core/utils/app_colors.dart';
+import 'package:fruits_hub/core/utils/app_images.dart';
+import 'package:fruits_hub/core/utils/app_text_styles.dart';
+import 'package:fruits_hub/core/widgets/custom_button.dart';
+import 'package:fruits_hub/core/widgets/custom_text_field.dart';
+import 'package:fruits_hub/features/auth/presentation/views/widgets/or_divider.dart';
+import 'package:fruits_hub/features/auth/presentation/views/widgets/social_login_button.dart';
+
+import 'dont_have_account_widget.dart';
+
+class LoginViewBody extends StatelessWidget {
+  const LoginViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: kHorizintalPadding,
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 24,
+              ),
+              const CustomTextFormField(
+                hintText: 'البريد الالكتروني',
+                textInputType: TextInputType.emailAddress,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              const CustomTextFormField(
+                suffixIcon: Icon(
+                  Icons.remove_red_eye,
+                  color: Color(0xffC9CECF),
+                ),
+                hintText: 'كلمة المرور',
+                textInputType: TextInputType.visiblePassword,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'نسيت كلمة المرور؟',
+                    style: TextStyles.semiBold13.copyWith(
+                      color: AppColors.lightPrimaryColor,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 33,
+              ),
+              CustomButton(
+                onPressed: () {},
+                text: 'تسجيل دخول',
+              ),
+              const SizedBox(
+                height: 33,
+              ),
+              const DontHaveAnAccountWidget(),
+              const SizedBox(
+                height: 33,
+              ),
+              const OrDivider(),
+              const SizedBox(
+                height: 16,
+              ),
+              SocialLoginButton(
+                onPressed: () {},
+                image: Assets.imagesGoogleIcon,
+                title: 'تسجيل بواسطة جوجل',
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              SocialLoginButton(
+                onPressed: () {},
+                image: Assets.imagesApplIcon,
+                title: 'تسجيل بواسطة أبل',
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              SocialLoginButton(
+                onPressed: () {},
+                image: Assets.imagesFacebookIcon,
+                title: 'تسجيل بواسطة فيسبوك',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
